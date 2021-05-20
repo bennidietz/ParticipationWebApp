8fc96cdfd83a74f0167333a1db5a499c3cdbf995
christian-terbeck <mail@christian-terbeck.de>
2021-05-20 10:07:48 +0200

    added missing translations and adjusted internal area

---
+++ resources/lang/de/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => 'Fehler. Bitte stellen Sie sicher, dass Ihre E-Mail Adresse und Ihr Passwort korrekt sind.',
+    'password' => 'Das eingegebene Passwort ist nicht korrekt.',
+    'throttle' => 'Zu viele Loginversuche. Versuchen Sie es bitte in :seconds Sekunden nochmal.',
+];
