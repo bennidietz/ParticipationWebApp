e25bb384d12a688559edac55e2003f0de7494498
Christian Terbeck <mail@christian-terbeck.de>
2021-06-05 11:02:36 +0200

    api validation errors are now also being returned as json

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Auth;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Validator;
 
 class ApiController
 {
@@ -13,13 +14,17 @@ class ApiController
     */
     public function login(Request $request)
     {
-        $validatedData = $request->validate([
+        $validator = Validator::make($request->all(), [
             'email' => 'required|email:rfc,dns',
             'password' => 'required',
         ]);
 
-        if (Auth::attempt($validatedData)) {
-            return User::where('email', '=', $validatedData['email'])->first();
+        if ($validator->fails()) {
+           return response()->json($validator->errors(), 422);
+        }
+
+        if (Auth::attempt($validator->valid())) {
+            return User::where('email', '=', $validator->valid()['email'])->first();
         }
 
         abort(401);
@@ -30,12 +35,16 @@ class ApiController
     */
     public function register(Request $request)
     {
-        $validatedData = $request->validate([
+        $validator = Validator::make($request->all(), [
             'name' => 'required',
             'email' => 'required|email:rfc,dns|unique:users',
             'password' => 'required|min:8|confirmed',
         ]);
 
-        return User::create($validatedData);
+        if ($validator->fails()) {
+           return response()->json($validator->errors(), 422);
+        }
+
+        return User::create($validator->valid());
     }
 }
