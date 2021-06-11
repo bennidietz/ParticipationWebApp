0ba9af3702f390d62c50e06c7b64ec9efb8e9a74
cherry13579 <lia.kirsch@googlemail.com>
2021-06-11 18:08:26 +0200

    Change

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -38,7 +38,7 @@
             <div class="w-5/6 lg:w-4/5 max-w-full">
                 <div class="w-full md:w-3/5">
                     <h2 class="mb-4 text-2xl md:text-4xl">{{ __('Lorem ipsum dolor') }}</h2>
-                    <p class="mb-4">{{ __('Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.') }}</p>
+                    <p class="mb-4">{{ __('Änderung ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.') }}</p>
                     <p class="mb-6">{{ __('Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.') }}</p>
                     <button class="py-2 px-4 text-sm uppercase border rounded transition hover:bg-white hover:text-black">{{ __('Mach mit!') }}</button>
                 </div>
