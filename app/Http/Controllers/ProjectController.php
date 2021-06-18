886def84020c2eb445ae051c3a01b363737b7edd
Benni Dietz <b_diet05@uni-muenster.de>
2021-06-18 21:09:38 +0200

    Basic input form for project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -10,6 +10,7 @@ use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Validator;
--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ $project->id ? __('Edit contact') : __('Create contact') }}
+            {{ $project->id ? __('Projekt bearbeiten') : __('Projekt erstellen') }}
         </h2>
     </x-slot>
 
@@ -10,84 +10,78 @@
         @if ($project->id)
             @method('PUT')
         @endif
-        <div>
-            <label for="title">{{ __('Name') }}</label>
-            <input id="title" type="text" name="title" value="{{ old('name', $project->name) }}" placeholder="optional" />
-        </div>
-        <div>
-            <label for="description">{{ __('Beschreibung') }}</label>
-            <input id="description" type="text" name="description" value="{{ old('name', $project->description) }}" placeholder="optional" />
-        </div>
-        <div>
-            <label for="content">{{ __('Inhalt') }}</label>
-            <input id="content" type="text" name="content" value="{{ old('name', $project->content) }}" placeholder="optional" />
-        </div>
+        <div style="width: 20%; margin: 0 auto; margin-top: 50px">
+            <div>
+                <label for="title">{{ __('Name') }}</label>
+                <input id="name" type="text" name="name" value="{{ old('name', $project->name) }}" placeholder="optional" />
+            </div>
+            <div>
+                <label for="description">{{ __('Beschreibung') }}</label>
+                <input id="description" type="text" name="description" value="{{ old('name', $project->description) }}" placeholder="optional" />
+            </div>
+            <div>
+                <label for="content">{{ __('Inhalt') }}</label>
+                <input id="content" type="text" name="content" value="{{ old('name', $project->content) }}" placeholder="optional" />
+            </div>
 
-        <div>
-            <label for="visible">{{ __('Sichtbar') }}</label>
-            <select id="visible" name="visible" size="1" required>
-                @if (!old('visible', $project->visible))
-                    <option value="" disabled selected>{{ __('Please select') }}</option>
-                @else
-                    <option value="" disabled>{{ __('Please select') }}</option>
-                @endif
-                @if (old('visible', $project->visible) == 0)
-                    <option value="Ja" selected>{{ __('Ja') }}</option>
-                @else
-                    <option value="Ja">{{ __('Ja') }}</option>
-                @endif
-                @if (old('visible', $project->visible) == 1)
-                    <option value="Nein" selected>{{ __('Nein') }}</option>
-                @else
-                    <option value="Nein">{{ __('Nein') }}</option>
-                @endif
-            </select>
-        </div>
-        <div>
-            <label for="visible">{{ __('Typ') }}</label>
-            <select id="is_event" name="is_event" size="1" required>
-                @if (!old('is_event', $project->is_event))
-                    <option value="" disabled selected>{{ __('Please select') }}</option>
-                @else
-                    <option value="" disabled>{{ __('Please select') }}</option>
-                @endif
-                @if (old('is_event', $project->is_event) == 0)
-                    <option value="Event" selected>{{ __('Event') }}</option>
-                @else
-                    <option value="Event">{{ __('Event') }}</option>
-                @endif
-                @if (old('is_event', $project->is_event) == 1)
-                    <option value="Project" selected>{{ __('Project') }}</option>
-                @else
-                    <option value="Project">{{ __('Project') }}</option>
-                @endif
-            </select>
-        </div>
-        <div>
-            <label for="contact-preference">{{ __('Contact preference') }}</label>
-            <select id="contact-preference" name="contact_preference" size="1">
-                @if (!old('contact_preference', $project->contact_preference))
-                    <option value="" selected>{{ __('no preference') }}</option>
-                @else
-                    <option value="">{{ __('no preference') }}</option>
-                @endif
-                @if (old('contact_preference', $project->contact_preference) == 'phone')
-                    <option value="phone" selected>{{ __('Phone') }}</option>
-                @else
-                    <option value="phone">{{ __('Phone') }}</option>
-                @endif
-                @if (old('contact_preference', $project->contact_preference) == 'email')
-                    <option value="email" selected>{{ __('Email') }}</option>
-                @else
-                    <option value="email">{{ __('Email') }}</option>
-                @endif
-            </select>
-        </div>
-        <div>
-            <button>{{ __('Save') }}</button>
+            <div>
+                <label for="visible">{{ __('Sichtbar') }}</label>
+                <select id="visible" name="visible" size="1" required>
+                    @if (!old('visible', $project->visible))
+                        <option value="" disabled selected>{{ __('Please select') }}</option>
+                    @else
+                        <option value="" disabled>{{ __('Please select') }}</option>
+                    @endif
+                    @if (old('visible', $project->visible) == 0)
+                        <option value="1" selected>{{ __('Ja') }}</option>
+                    @else
+                        <option value="1">{{ __('Ja') }}</option>
+                    @endif
+                    @if (old('visible', $project->visible) == 1)
+                        <option value="0" selected>{{ __('Nein') }}</option>
+                    @else
+                        <option value="0">{{ __('Nein') }}</option>
+                    @endif
+                </select>
+            </div>
+            <div>
+                <label for="visible">{{ __('Typ') }}</label>
+                <select id="is_event" name="is_event" size="1" required>
+                    @if (!old('is_event', $project->is_event))
+                        <option value="" disabled selected>{{ __('Please select') }}</option>
+                    @else
+                        <option value="" disabled>{{ __('Please select') }}</option>
+                    @endif
+                    @if (old('is_event', $project->is_event) == 0)
+                        <option value="1" selected>{{ __('Event') }}</option>
+                    @else
+                        <option value="1">{{ __('Event') }}</option>
+                    @endif
+                    @if (old('is_event', $project->is_event) == 1)
+                        <option value="0" selected>{{ __('Project') }}</option>
+                    @else
+                        <option value="0">{{ __('Project') }}</option>
+                    @endif
+                </select>
+            </div>
+            <div>
+                <label for="visible">{{ __('Datum Beginn') }}</label>
+                <input type="date" id="start" name="start_time"
+                       value="<?php echo date('Y-m-d'); ?>"
+                       value="<?php echo date('Y-m-d'); ?>" max="2022-12-31">
+            </div>
+            <div>
+                <label for="visible">{{ __('Datum Ende') }}</label>
+                <input type="date" id="end" name="end_time"
+                       value="<?php echo date('Y-m-d'); ?>"
+                       value="<?php echo date('Y-m-d'); ?>" max="2022-12-31">
+            </div>
+            <div style="margin: 20px;">
+                <button>{{ __('Save') }}</button>
+                <a value="cancel" href="{{ route('projects.index') }}">
+                    <button>{{ __('Cancel') }}</button>
+                </a>
+            </div>
         </div>
     </form>
-    <a href="{{ route('projects.index') }}">
-        <button>{{ __('Cancel') }}</button>
-    </a>
 </x-app-layout>
