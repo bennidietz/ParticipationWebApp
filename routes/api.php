82909c20e7afa6cde66f6d95def7734f4733d9ea
Christian Terbeck <mail@christian-terbeck.de>
2021-06-29 17:41:25 +0200

    added new api routes

--- routes/api.php
+++ routes/api.php
@@ -33,9 +33,11 @@ Route::resource('polygon', PolygonController::class);
 Route::resource('asset', AssetController::class);
 
 Route::get('/suggestion/{suggestion}/comment', [SuggestionController::class, 'getCommentsOfSuggestion']);
+Route::get('/suggestion/{suggestion}/vote', [SuggestionController::class, 'getVotesOfSuggestion']);
 Route::resource('suggestion', SuggestionController::class);
 
 Route::get('/comment/{comment}/comment', [CommentController::class, 'getCommentsOfComment']);
+Route::get('/comment/{comment}/vote', [CommentController::class, 'getVotesOfComment']);
 Route::resource('comment', CommentController::class);
 
 Route::resource('vote', VoteController::class);
