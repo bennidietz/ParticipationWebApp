a6acd1de738ad5839bcc0f7cf783c0bf92ef65c0
Christian Terbeck <mail@christian-terbeck.de>
2021-07-10 11:51:36 +0200

    visual update for about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,38 +2,38 @@
 
     <section class="w-full min-h-screen bg-cover text-white flex items-stretch" style="background-image: url('{{ asset('img/about/Corrensstrasse_1.png') }}');">
 
-        <div class="w-full p-8 bg-gray-900 bg-opacity-30 flex justify-center items-center">
-            <div class="w-full p-8 bg-green-500 bg-opacity-70 flex justify-center items-left">
-                <div class="w-full md:w-3/5">
-                    <h2 class="mb-4 text-2xl md:text-5xl">{{ __('Die Corrensstraße im urbanem Wissensquartier') }}</h2>
-                    <p class="mb-4 text-2xl md:text-2xl">{{ __('Die Corrensstraße führt von der Von-Esmach-Straße zum Horstmarer Landweg. Sie ist ein Verbindungsglied der Urbanen Wissenschaftsstadt. Die Zukunft Münsters ist eng verknüpft mit der Entwicklung der Wissenschaft in der Stadt. Gleichzeitig sind die urbanen Qualitäten Münsters ein wesentlicher Standortfaktor für die Entwicklung der Wissenschafts- und Forschungsinstitutionen. Wie kann es gelingen ein Quartier zu entwickeln in dem Arbeiten, Forschen, Freizeit und Wohnen Hand in Hand gehen?.') }}</p>
-                    <p class="mb-4 text-2xl md:text-2xl">{{ __('Sie wollen helfen den städtischen Raum zukunftssicher zu gestalten?   ') }}</p>
-                    <p class="mb-4 text-1xl md:text-2xl">{{ __(' Sie wollen der Corensstraße eine neue Aufenthaltsqualität zu geben?') }}</p>
+        <div class="w-full p-8 bg-gray-900 bg-opacity-40 flex justify-center items-center">
+            <div class="w-full p-8 flex justify-center items-left">
+                <div class="w-full md:w-3/5 text-lg">
+                    <h1 class="mb-4 text-2xl">{{ __('Die Corrensstraße im urbanem Wissensquartier') }}</h1>
+                    <p class="mb-4">{{ __('Die Corrensstraße führt von der Von-Esmach-Straße zum Horstmarer Landweg. Sie ist ein Verbindungsglied der Urbanen Wissenschaftsstadt. Die Zukunft Münsters ist eng verknüpft mit der Entwicklung der Wissenschaft in der Stadt. Gleichzeitig sind die urbanen Qualitäten Münsters ein wesentlicher Standortfaktor für die Entwicklung der Wissenschafts- und Forschungsinstitutionen. Wie kann es gelingen ein Quartier zu entwickeln in dem Arbeiten, Forschen, Freizeit und Wohnen Hand in Hand gehen?.') }}</p>
+                    <p class="mb-4">{{ __('Sie wollen helfen den städtischen Raum zukunftssicher zu gestalten?   ') }}</p>
+                    <p class="mb-4">{{ __(' Sie wollen der Corensstraße eine neue Aufenthaltsqualität zu geben?') }}</p>
                 </div>
             </div>
-            
-         
+
+
         </div>
 
     </section>
 
     <section class="w-full min-h-screen bg-cover text-white flex items-stretch" style="background-image: url('{{ asset('img/about/Corrensstrasse_2.png') }}');">
 
-        <div class="w-full p-8 bg-gray-900 bg-opacity-30 flex justify-center items-center">
-            <div class="w-full p-8 bg-red-400 bg-opacity-70 flex justify-center items-left">
-                <div class="w-full md:w-3/5 " >
-                    <h1 class="mb-4 text-2xl md:text-5xl">{{ __('Machen Sie mit') }}</h1>
-                    <p class="mb-4 text-1xl md:text-2xl">{{ __('Was sind Ihre Anforderungen an ein Quartier von morgen? ') }}</p>
-                    <p class="mb-4 text-1xl md:text-2xl">{{ __('Wie schaffen wir Aufenthaltsqualität an und auf der Corrensstraße? ') }}</p>
-                    <p class="mb-4 text-1xl md:text-2xl">{{ __('Wie wollen Sie, dass die Corrensstraße in Zukunft aussieht?') }}</p>
-                    <p class="mb-4 text-1xl md:text-2xl">{{ __('Ihre Meinung zählt. Schauen Sie rein und gestalten Sie die Corrensstraße von Morgen!') }}</p>
-                    <button class="py-5 px-7 text-sm uppercase border rounded transition hover:bg-white hover:text-black">{{ __('Hier geht’s zum Projekt') }}</button>
+        <div class="w-full p-8 bg-gray-900 bg-opacity-40 flex justify-center items-center">
+            <div class="w-full p-8 flex justify-center items-left">
+                <div class="w-full md:w-3/5 text-lg">
+                    <h1 class="mb-4 text-2xl">{{ __('Machen Sie mit!') }}</h1>
+                    <p class="mb-4">{{ __('Was sind Ihre Anforderungen an ein Quartier von morgen? ') }}</p>
+                    <p class="mb-4">{{ __('Wie schaffen wir Aufenthaltsqualität an und auf der Corrensstraße? ') }}</p>
+                    <p class="mb-4">{{ __('Wie wollen Sie, dass die Corrensstraße in Zukunft aussieht?') }}</p>
+                    <p class="mb-4">{{ __('Ihre Meinung zählt. Schauen Sie rein und gestalten Sie die Corrensstraße von Morgen!') }}</p>
+                    <button class="py-3 px-2 text-sm uppercase border rounded transition hover:bg-white hover:text-black">{{ __('Hier geht’s zum Projekt') }}</button>
                </div>
             </div>
         </div>
 
     </section>
 
-   
+
 
 </x-guest-layout>
