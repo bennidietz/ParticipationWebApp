fd49b64027159a3d4afae7bbe3248dcc5af05b6d
Christian Terbeck <mail@christian-terbeck.de>
2021-07-10 12:11:07 +0200

    updated projects page

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
 
-    <div class="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+    <div class="max-w-7xl mx-auto pt-24 pb-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
         <x-project :image="asset('img/projects/pexels-startup-stock-photos-7376.jpg')"
                     title="Project title"
                     description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quo quam, sint omnis iusto numquam laboriosam unde rem suscipit necessitatibus?"
