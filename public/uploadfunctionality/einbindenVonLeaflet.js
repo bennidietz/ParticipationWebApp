3620889bd34e1b13a3f42e66dc32870ecdb27e1b
JudithBre <61976163+JudithBre@users.noreply.github.com>
2021-09-12 18:09:15 +0200

    How to get the coordinates of the markers? Markers, popups are visible.

--- public/uploadfunctionality/einbindenVonLeaflet.js
+++ public/uploadfunctionality/einbindenVonLeaflet.js
@@ -13,8 +13,37 @@ const start_latlng = [lat, lon];
 
 var map = L.map("mapdiv", {
     center: start_latlng,
-    zoom: 16,
+    zoom: 17,
 });
+
+// Darstellen der Marker auf der Leaflet-Karte und ermoeglichen der Auswahl durch User
+// Wenn die Koordinaten der Marker bekannt sind, dann muessen die Koordinaten nicht erst aus der Datenbank abgefragt werden --> Zeitersparnis!
+var marker1 = L.marker([51.96897313542579, 7.596425373765546]).addTo(map)
+    .bindPopup('Standort 1');
+var marker2 = L.marker([51.96933035015426, 7.595885496155732]).addTo(map)
+    .bindPopup('Standort 2');
+var marker3 = L.marker([51.968100900156045, 7.595874767311157]).addTo(map)
+    .bindPopup('Standort 3');
+var marker4 = L.marker([51.96955508466853, 7.596958379699424]).addTo(map)
+    .bindPopup('Standort 4');
+var marker5 = L.marker([51.969121591901335, 7.5966947740650665]).addTo(map)
+    .bindPopup('Standort 5');
+
+// Koordinaten der Marker zwischenspeichern
+var showCoordinates1 = marker1.getLatLng();
+var showCoordinates2 = marker2.getLatLng();
+var showCoordinates3 = marker3.getLatLng();
+var showCoordinates4 = marker4.getLatLng();
+var showCoordinates5 = marker5.getLatLng();
+
+// Grundidee: Bei einem Klick auf einen Marker werden die Koordinaten "ausgelesen" und als Informtation mit an den Server gesendet (POST Asset)
+
+// Koordinaten des ausgewaehlten Markers ermitteln
+map.on("click", function(e){
+      var showCoordinates = marker1.getLatLng();
+      console.log(showCoordinates);
+      document.getElementById("gibKoordinatenVomMarker").innerHTML = showCoordinates;
+         });
 
 var osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 18,
