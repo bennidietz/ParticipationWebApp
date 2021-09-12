6eac840eb398d6bf6307d081e35868c39e262580
JudithBre <61976163+JudithBre@users.noreply.github.com>
2021-09-12 22:42:22 +0200

    Leaflet map visible again. Included scripts were probably outdated.

--- public/uploadfunctionality/einbindenVonLeaflet.js
+++ public/uploadfunctionality/einbindenVonLeaflet.js
@@ -12,22 +12,22 @@ const lon = 7.596425373765546;
 const start_latlng = [lat, lon];
 
 var map = L.map("map", {
-    center: start_latlng,
-    zoom: 17,
+  center: start_latlng,
+  zoom: 17,
 });
 
 // Darstellen der Marker auf der Leaflet-Karte und ermoeglichen der Auswahl durch User
 // Wenn die Koordinaten der Marker bekannt sind, dann muessen die Koordinaten nicht erst aus der Datenbank abgefragt werden --> Zeitersparnis!
 var marker1 = L.marker([51.96897313542579, 7.596425373765546]).addTo(map)
-    .bindPopup('Standort 1');
+.bindPopup('Standort 1');
 var marker2 = L.marker([51.96933035015426, 7.595885496155732]).addTo(map)
-    .bindPopup('Standort 2');
+.bindPopup('Standort 2');
 var marker3 = L.marker([51.968100900156045, 7.595874767311157]).addTo(map)
-    .bindPopup('Standort 3');
+.bindPopup('Standort 3');
 var marker4 = L.marker([51.96955508466853, 7.596958379699424]).addTo(map)
-    .bindPopup('Standort 4');
+.bindPopup('Standort 4');
 var marker5 = L.marker([51.969121591901335, 7.5966947740650665]).addTo(map)
-    .bindPopup('Standort 5');
+.bindPopup('Standort 5');
 
 // Koordinaten der Marker zwischenspeichern
 var showCoordinates1 = marker1.getLatLng();
@@ -40,10 +40,10 @@ var showCoordinates5 = marker5.getLatLng();
 
 // Koordinaten des ausgewaehlten Markers ermitteln
 map.on("click", function(e){
-      var showCoordinates = marker1.getLatLng();
-      console.log(showCoordinates);
-      document.getElementById("gibKoordinatenVomMarker").innerHTML = showCoordinates;
-         });
+  var showCoordinates = marker1.getLatLng();
+  console.log(showCoordinates);
+  document.getElementById("gibKoordinatenVomMarker").innerHTML = showCoordinates;
+});
 
 var osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 18,
@@ -60,14 +60,14 @@ var mapbox = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?a
 }).addTo(map);
 
 var opentopomap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-	maxZoom: 17,
-	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+  maxZoom: 17,
+  attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 });
 
 var baseMaps = {
-    "opentopomap" : opentopomap,
-    "osm": osm,
-    "mapbox": mapbox
+  "opentopomap" : opentopomap,
+  "osm": osm,
+  "mapbox": mapbox
 };
 
 L.control.layers(baseMaps).addTo(map);
