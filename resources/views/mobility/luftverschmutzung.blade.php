b86c19b958337b7b1228b6960ef27c46cd71fbb4
Benni Dietz <b_diet05@uni-muenster.de>
2021-09-14 11:36:04 +0200

    Fix spelling

--- resources/views/mobility/luftverschmutzung.blade.php
+++ resources/views/mobility/luftverschmutzung.blade.php
@@ -3,16 +3,16 @@
     <section class="pt-24 pb-8 max-w-7xl mx-auto">
         <h2 class="mb-4 text-3xl font-bold uppercase tracking-wider text-green-600">Luftverschmutzung</h2>
         <div class="text-xl w-2/3 leading-relaxed tracking-wide">
-            <br><h3 class='font-bold'>Auf der Straße der Zukunft kannst du saubere Luft atmen ohne, dass es dei-ner Gesundheit schadet. </h3>
+            <br><h3 class='font-bold'>Auf der Straße der Zukunft kannst du saubere Luft atmen ohne, dass es deiner Gesundheit schadet. </h3>
             <br>
-            Das klingt absurd? Doch leider ist dies die Realität. Das findet auch Anna Pribil, eine Umweltberaterin und Psychologin aus der Steiermark in Österreich: “Ich se-he total ein Problem in der Luftqualität. Das ist ein Gesundheitsrisiko für die Menschen”.
-            Urbane Räume sind vermehrt von starker Luftverschmutzung betroffen, sodass jeder Atemzug deiner Gesundheit schadet. Das Europäische Umweltamt gibt so-gar an, dass jährlich 400.000 Menschen an den Folgen der Luftverschmutzung sterben und dabei nimmt der Anteil des urbane Raums als Wohnort zu.
+            Das klingt absurd? Doch leider ist dies die Realität. Das findet auch Anna Pribil, eine Umweltberaterin und Psychologin aus der Steiermark in Österreich: “Ich sehe total ein Problem in der Luftqualität. Das ist ein Gesundheitsrisiko für die Menschen”.
+            Urbane Räume sind vermehrt von starker Luftverschmutzung betroffen, sodass jeder Atemzug deiner Gesundheit schadet. Das Europäische Umweltamt gibt sogar an, dass jährlich 400.000 Menschen an den Folgen der Luftverschmutzung sterben und dabei nimmt der Anteil des urbane Raums als Wohnort zu.
 
             <br><br><h3 class='font-bold'>Woher kommt der Feinstaub?</h3>
-            Durch unterschiedliche Quellen wird Feinstaub produziert. Laut des Umweltbun-desamts geht dabei fast die Hälfte allein auf den Auspuff und den Reifenabrieb im Verkehr zurück. Aufgrund diesen immensen Beitrags, gilt es für die Straße der Zukunft möglichst den Verkehr so zu regulieren, dass die Feinstaubbelastung minimiert wird.
+            Durch unterschiedliche Quellen wird Feinstaub produziert. Laut des Umweltbundesamts geht dabei fast die Hälfte allein auf den Auspuff und den Reifenabrieb im Verkehr zurück. Aufgrund diesen immensen Beitrags, gilt es für die Straße der Zukunft möglichst den Verkehr so zu regulieren, dass die Feinstaubbelastung minimiert wird.
 
             <br><br><h3 class='font-bold'>Was macht der Feinstaub?</h3>
-            Diese kleinen Partikel gelangen durch die Atemluft in den Körper des Menschen und lagern sich ab bis schließlich Organe Schäden davontragen. Schwere Er-krankungen wie Herz- und Lungenerkrankungen sowie Krebs und Demenz kön-nen die Folgen sein. Dabei werden diese von der unterschiedlichen Größe der Partikel begünstigt, da sie unterschiedlich tief in den Körper vordringen können: PM1, PM2.5 und PM10.
+            Diese kleinen Partikel gelangen durch die Atemluft in den Körper des Menschen und lagern sich ab bis schließlich Organe Schäden davontragen. Schwere Erkrankungen wie Herz- und Lungenerkrankungen sowie Krebs und Demenz können die Folgen sein. Dabei werden diese von der unterschiedlichen Größe der Partikel begünstigt, da sie unterschiedlich tief in den Körper vordringen können: PM1, PM2.5 und PM10.
 
 
             <br><br><img src="<?php echo e(asset('img/content/mobility/luftverschmutzung_1.png')); ?>" alt="Versiegelung" />
@@ -32,13 +32,13 @@
             Eine Möglichkeit besteht darin, die Straße zu bewässern, sodass der Feinstaub erst gar nicht aufgewirbelt werden kann. Kritiker stellen dem jedoch gegenüber, dass externe Einflüsse wie Wind den Effekt vermindern.
 
             <br><br><h3 class='font-bold'>Filteranlagen</h3>
-            Das Angebot an modernen Filteranlagen hat sich in den letzten Jahren deutlich ausgeweitet und geht von mechanischen über biologische Anlagen wie bei-spielsweise Moose und Algen. Die Anwendung von Moosen rückt dabei immer mehr in den Mittelpunkt, aufgrund ihrer natürlichen Eigenschaft der Nutzung von Feinstaub als eigenes Nahrungsmittel.
+            Das Angebot an modernen Filteranlagen hat sich in den letzten Jahren deutlich ausgeweitet und geht von mechanischen über biologische Anlagen wie beispielsweise Moose und Algen. Die Anwendung von Moosen rückt dabei immer mehr in den Mittelpunkt, aufgrund ihrer natürlichen Eigenschaft der Nutzung von Feinstaub als eigenes Nahrungsmittel.
 
             <br><br><h3 class='font-bold'>Belüftung</h3>
-            Insbesondere in isolierten urbanen Bereichen durch zum Beispiel Tunnel stellen Belüftungen eine Möglichkeit dar. Dennoch dienen diese lediglich dazu den Feinstaub abzutransportieren, denn dieser gelangt schließlich erneut in die At-mosphäre. Fraglich ist demnach, ob diese Lösung tatsächlich als Lösung betitelt werden sollte.
+            Insbesondere in isolierten urbanen Bereichen durch zum Beispiel Tunnel stellen Belüftungen eine Möglichkeit dar. Dennoch dienen diese lediglich dazu den Feinstaub abzutransportieren, denn dieser gelangt schließlich erneut in die Atmosphäre. Fraglich ist demnach, ob diese Lösung tatsächlich als Lösung betitelt werden sollte.
 
             <br><br><h3 class='font-bold'>Uns sonst noch?</h3>
-            Um den Feinstaub auf unseren Straßen zu reduzieren bedarf es innovativen Ideen. Anna Pribil hat dazu eine Vision anschaulich verfasst wie die Straße der Zukunft im Sinne der Mobilitätswende aussehen kann. Wenn Sie mehr dazu le-sen wollen, folgen Sie gerne diesem <a class="text-blue-800 hover:underline" href="https://www.annapribil.com/post/mobilit%C3%A4tswende-was-es-braucht">Link</a>.
+            Um den Feinstaub auf unseren Straßen zu reduzieren bedarf es innovativen Ideen. Anna Pribil hat dazu eine Vision anschaulich verfasst wie die Straße der Zukunft im Sinne der Mobilitätswende aussehen kann. Wenn Sie mehr dazu lesen wollen, folgen Sie gerne diesem <a class="text-blue-800 hover:underline" href="https://www.annapribil.com/post/mobilit%C3%A4tswende-was-es-braucht">Link</a>.
 
             <br><br><h3 class='font-bold'>Weitere interessante Informationen:</h3>
             Dr. Harald Frey (Universitätsprof. an der TU Wien, Verkehrsexperte) über die Verkehrswende: <a class="text-blue-800 hover:underline" href="https://www.youtube.com/watch?v=BygMUJLN5Ho">https://www.youtube.com/watch?v=BygMUJLN5Ho</a>
@@ -49,7 +49,6 @@
                 Freudenberg (o. J.): Feinstaub. Winzige Partikel. Hohes Gesundheitsrisiko. Online unter: <a class="text-blue-800 hover:underline" href="https://micronairblue.com/de/feinstaubbelastung-kfz-innenraumfilter">https://micronairblue.com/de/feinstaubbelastung-kfz-innenraumfilter</a> (abgerufen 08.09.21)<br><br>
                 Umweltbundesamt (2009): Feinstaubbelastung in Deutschland. Online unter: <a class="text-blue-800 hover:underline" href="https://www.umweltbundesamt.de/sites/default/files/medien/publikation/long/3565.pdf">https://www.umweltbundesamt.de/sites/default/files/medien/publikation/long/3565.pdf</a> (abgerufen 07.09.21)
             </div>
-            <a class="text-blue-800 hover:underline" href=""></a>
         </div>
     </section>
 
--- resources/views/mobility/versiegelung.blade.php
+++ resources/views/mobility/versiegelung.blade.php
@@ -4,19 +4,19 @@
         <h2 class="mb-4 text-3xl font-bold uppercase tracking-wider text-green-600">Versiegelung</h2>
         <div class="text-xl w-2/3 leading-relaxed tracking-wide">
             Flächenversiegelung geht mit einer Vielzahl negativer Folgen für die Umwelt einher (Umweltbundesamt).<br>
-            Deshalb wird das Zubetonieren oder -asphaltieren ehemals fruchtbarer Flächen auf ein Minimum beschränkt, oder gar gänzlich vermieden. Überdimensioniert geplante Verkehrsräume, die einst versiegelt wurden werden zurückgebaut und machen Platz für Grünflächen, Versickerungsmulden, Mikrobiotope sowie bo-denschonende Rad- und Fußgängerwege. Die Kombination führt zu einer deutli-chen Verbesserung des allgemeinen Stadtklimas und der Biodiversität vor Ort.
+            Deshalb wird das Zubetonieren oder -asphaltieren ehemals fruchtbarer Flächen auf ein Minimum beschränkt, oder gar gänzlich vermieden. Überdimensioniert geplante Verkehrsräume, die einst versiegelt wurden werden zurückgebaut und machen Platz für Grünflächen, Versickerungsmulden, Mikrobiotope sowie bo-denschonende Rad- und Fußgängerwege. Die Kombination führt zu einer deutlichen Verbesserung des allgemeinen Stadtklimas und der Biodiversität vor Ort.
             <br><br><h3 class='font-bold'>Entwicklung der Versiegelungsflächen</h3>
             <img src="<?php echo e(asset('img/content/mobility/versiegelung_1.png')); ?>" alt="Versiegelung" />
             <span class="italic text-sm">Quelle: eigene Darstellung nach Daten des Statistischen Bundesamts</span>
             <br><br><h3 class='font-bold'>Begrünte Stadt</h3>
-            Eine Begrünung und Bepflanzung in unmittelbarer Straßennähe trägt nicht nur zu einer erheblichen optischen Aufwertung von Siedlungsräumen bei, sondern sorgt parallel dafür, dass eine städtische „Grüne Lunge“ gebildet wird. Gleichzei-tig werden die anliegenden Naturräume nachhaltig gestärkt, da neuer Lebens-raum geschaffen wird. Die Aufenthaltsqualität im Straßenraum steigt damit um ein Vielfaches.
+            Eine Begrünung und Bepflanzung in unmittelbarer Straßennähe trägt nicht nur zu einer erheblichen optischen Aufwertung von Siedlungsräumen bei, sondern sorgt parallel dafür, dass eine städtische „Grüne Lunge“ gebildet wird. Gleichzeitig werden die anliegenden Naturräume nachhaltig gestärkt, da neuer Lebens-raum geschaffen wird. Die Aufenthaltsqualität im Straßenraum steigt damit um ein Vielfaches.
 
             <br><br><h3 class='font-bold'>Versickerungsfreundliche Stadt</h3>
             Die Begrünung des Straßenraums wird kombiniert mit der Errichtung von soge-nannten Mulden-Rigolen-Systemen. Sie sorgen dafür, dass Einwirkungen auf den Wasserhaushalt verringert werden und Niederschlagswasser zuverlässig abläuft. Die im Rahmen von immer häufiger auftretenden Starkniederschlags- und Überschwemmungsereignissen entstehenden Risiken werden durch die Steigerung der Wasseraufnahmekapazität deutlich begrenzt. Auch Parkplatz- und Abstellflächen werden aus Materialien mit geringen Versiegelungsgeraden gebaut.<br><br>
             <img src="<?php echo e(asset('img/content/mobility/versiegelung_2.png')); ?>" alt="Versiegelung" />
             <span class="italic text-sm">Quelle: Tiefbauamt Stadt Münster</span>
             <br><br><h3 class='font-bold'>Recyclingorientierte Stadt</h3>
-            Falls im Rahmen der Erschließung neuer Wohn- oder Gewerbegebiete doch neue Infrastruktur benötigt wird – oder alte Straßenzüge erneuert werden – wer-den recycelte Straßenbaumaterialien für die Errichtung verwendet. Hierzu zählen zum Beispiel alte Reifen und Bauschutt. Das Schließen von Stoffkreisläufen ver-ringert zusätzliche Flächeninanspruchnahmen in der Rohstoffgewinnung.
+            Falls im Rahmen der Erschließung neuer Wohn- oder Gewerbegebiete doch neue Infrastruktur benötigt wird – oder alte Straßenzüge erneuert werden – werden recycelte Straßenbaumaterialien für die Errichtung verwendet. Hierzu zählen zum Beispiel alte Reifen und Bauschutt. Das Schließen von Stoffkreisläufen verringert zusätzliche Flächeninanspruchnahmen in der Rohstoffgewinnung.
 
             <br><br><h3 class='font-bold'>Fuß- und Radfreundliche Stadt</h3>
             Ein Verzicht auf die Nutzung des Automobils und das damit einhergehende Wegfallen der Notwendigkeit einer Straße gibt insbesondere in Wohnsiedlungen mehr Raum für andere Nutzungsmöglichkeiten der Mobilität. Benötigte Flächen für Fuß- und Radwege sind weniger flächenintensiv und geben mehr Spielraum für innovative, nachhaltige Gestaltungsmaßnahmen.
