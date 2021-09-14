b2634fa7c028f6607c5d83c15f39c420b60a35bf
JudithBre <61976163+JudithBre@users.noreply.github.com>
2021-09-14 09:33:18 +0200

    Limitation to basic functionality

--- public/uploadfunctionality/script.js
+++ public/uploadfunctionality/script.js
@@ -6,35 +6,25 @@
 // jshint -W083
 "use strict";
 
-// Abfrage der Marker aus der Datenbank
-// console.log("Abfrage der Marker starten");
-// let request = new XMLHttpRequest();
-// request.onload = function() {
-//const myObj2 = JSON.parse(this.responseText);
-//document.getElementById("demo").innerHTML = myObj2.name;
-// };
-// request.open("Get", "http://giv-project10.uni-muenster.de/api/marker", false);
-// request.send();
-//console.log("Get request wurde versendet.");
-
-// Umwandlung der JSON-Zeichenkette in ein Java-Script-Objekt
-//let objectParsed = JSON.parse(
-//  string,                       // JSON
-//  (key, value) => {             // Ersetzungsfunktion
-//    return value;               // Rückgabewert bestimmt Zielwert
-//  }
-//);
-
-// const myJSON = '{"name":"John", "age":31, "city":"New York"}';
-// const myObj = JSON.parse(myJSON);
-// document.getElementById("listeMarkerAuf").innerHTML = myObj.name;
-
 // Senden eines Assets an den Server
 function sendAsset(){
-  const goAsset = new XMLHttpRequest();
-  goAsset.onload = function() {
-    document.getElementById("name").innerHTML = this.responseText;
+  let goAsset = new XMLHttpRequest();
+  let myData = document.getElementByID("name").value;
+
+
+  // Event Handler fuer die Response vom Server
+  goAsset.onload = function () {
+    if (goAsset.status === 200) {
+      // Dateien wurden hochgeladen
+
+      document.querySelector("#result").innerHTML = goAsset.responseText;
+      uploadButton.innerHTML = 'Upload';
+
+    } else {
+      document.querySelector("#result").innerHTML = "Fehler beim Upload " + goAsset.responseText;
+    }
   };
-  goAsset.open("POST", "http://giv-project10.uni-muenster.de/api/asset");
+
+  goAsset.open("POST", "http://giv-project10.uni-muenster.de/api/asset", true);
   goAsset.send();
 }
