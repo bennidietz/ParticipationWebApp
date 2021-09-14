0cda3bfab2bae98a7af11469104dd0f9687a4f6a
Benni Dietz <b_diet05@uni-muenster.de>
2021-09-14 12:24:57 +0200

    Finalize Texts for mobility

--- resources/views/mobility/fahrrad.blade.php
+++ resources/views/mobility/fahrrad.blade.php
@@ -1,4 +1,40 @@
 <x-guest-layout>
 
-    <x-header-text-section header="Vorteile des Fahrrades gegenüber dem Auto" text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."></x-header-text-section>
+    <section class="pl-5 pt-24 pb-8 max-w-7xl mx-auto">
+        <h2 class="mb-4 text-3xl font-bold uppercase tracking-wider text-green-600">Vorteile des Fahrrades gegenüber dem Auto</h2>
+        <div class="text-xl w-2/3 leading-relaxed tracking-wide">
+            <br><h3 class='font-bold'>Auf der Straße der Zukunft ist das Fahrrad das vorrangig genutzte Verkehrsmittel und macht dabei die Vorteile gegenüber dem Auto erfahrbar.</h3>
+            <br>
+            Münster ist seit jeher als Fahrradstadt bekannt, so verwundert es wenig, dass das Fahrrad als Verkehrsmittel mit einem Anteil von fast 40% des Modal Splits* (zum Vergleich: Motorisierter Individualverkehr, d. h. Auto: 29%) das meistge-nutzte Verkehrsmittel in der westfälischen Metropole ist. Aber warum ist das so und welche Vorteile bringt das Fahrrad überhaupt mit sich?
+
+
+            <br><br><img src="<?php echo e(asset('img/content/mobility/fahrrad_1.jpg')); ?>" alt="Versiegelung" />
+            <span class="italic text-sm">Quelle: Veltop</span><br><br>
+            Mithilfe von Lastenrädern, die genauso in das Shared Mobility Konzept integriert werden können, können ebenfalls Lieferungen zwischen den Instituten erfolgen, für die ansonsten das Auto genutzt werden würde (vgl. Tretty, o. D.). Und: Je weniger Autos genutzt werden (müssen), desto weniger Parkplätze werden benötigt. Dieser Platz kann für die festen Leih- und Ladestationen genutzt werden. Zudem können dort sichere und überdachte Fahrradstellplätze für das eigene Fahrrad oder Lastenrad implementiert werden. Eine oder mehrere Selbst-Reparatur-/Check-Up-Stationen können darüber hinaus als „Repair-Cafés“ lang-fristig an den umliegenden Fachbereichen eingerichtet werden. Solche Repair-Cafés ermöglichen neben dem Wissensaustausch die Entstehung sozialer Treffpunkte. Voraussetzungen sind hierfür genug personelle Ressourcen oder sogar Förderungen.
+
+            <br><br><h3 class='font-bold'>Ökologisch</h3>
+            Einer der offensichtlichsten Stärken des Fahrrades liegt dabei in seiner Umweltverträglichkeit, denn vom Radfahren selbst gehen keinerlei Emissionen aus. Darüber hinaus werden sowohl für die Herstellung als auch den Betrieb eines Fahrrades weitaus weniger Ressourcen benötigt. Zusätzlich ist das Fahrrad auch geräuscharm und wirkt sich auch in dieser Hinsicht positiv auf die Emissionen aus. Gleichzeitig nimmt das Fahrrad auch deutlich weniger Verkehrsfläche ein, sodass etwa die Stellplätze eine deutlich geringere Größe aufweisen müssen und diese Fläche im anderen Sinne genutzt werden kann.
+            <br><br><h3 class='font-bold'>Schnell und gesund</h3>
+            Dieser Aspekt mag auf den ersten Blick eigenartig klingen, aber das Fahrrad ist in seiner Schnelligkeit in der Stadt und besonders auf Kurzstrecken (< 5 km) un-schlagbar. Durch die Bewegung während des Radfahrens wird dem verbreiteten Bewegungsmangel entgegengewirkt, gleichzeitig bringen sie eine deutliche Steigerung des persönlichen Wohlbefindens mit sich. Dabei überwiegen diese Aspekte den oftmals entgegengestellten Argumenten wie etwa das Unfallrisiko. Dabei lässt sich durch eine passgenaue Infrastruktur, wie sie in Münster bspw. durch die Fahrradstraße 2.0 forciert wird, auch weiter an der Verringerung des Risikos arbeiten.
+            <br><br><h3 class='font-bold'>Günstig</h3>
+            Das Fahrrad ist nicht nur ökologisch, schnell und gesund, sondern zugleich auch noch günstig! Das gilt nicht nur für die Anschaffung sondern auch für den Betrieb. Bereits die Anschaffung ist meist deutlich günstiger als bspw. bei einem Auto und auch die Nutzung ist mit ca. 10 ct/gefahrenem Kilometer günstig. Dabei müssen auch die gesamtgesellschaftlichen Kosten einbezogen werden, die sich etwa aus Umwelteinwirkungen oder Infrastrukturmaßnahmen zusammensetzen.
+            Zu guter Letzt ist das Fahrradfahren auch noch angesagt, macht Spaß und leistet einen Beitrag für die Mobilität der Zukunft sowie eine lebenswerte Stadt.
+            Neben diesen genannten Aspekten gibt es noch unzählige weitere Gründe, das Fahrrad häufiger zu nutzen - besonders bei kurzen Strecken!<br><br>
+            Probiere dich auch gerne an den Lastenrädern - eine echte Alternative zum Transport ohne Auto!<br><br>
+            *Anteil der Wege, die in der jeweiligen Stadt mit dem entsprechenden Verkehrs-mittel zurückgelegt werden<br><br>
+
+
+            <br><br><div class="italic">
+                <span class="font-bold">Weitere Informationen:</span><br><br>
+                Vorteile des Rades (Umweltbundesamt): <a class="text-blue-800 hover:underline" href="https://repository.difu.de/jspui/bitstream/difu/256760/1/DS1918.pdf">https://repository.difu.de/jspui/bitstream/difu/256760/1/DS1918.pdf</a><br><br>
+                Neue Qualitätsstandards für Fahrradstraßen (Stadt Münster): <a class="text-blue-800 hover:underline" href="https://www.stadt-muenster.de/verkehrsplanung/mit-dem-rad/fahrradstrassen/qualitaetsstandards">https://www.stadt-muenster.de/verkehrsplanung/mit-dem-rad/fahrradstrassen/qualitaetsstandards</a><br><br>
+                <span class="font-bold">Quellen:</span><br><br>
+                ALLGEMEINER DEUTSCHER FAHRRAD CLUB (2018): So geht Verkehrswende - Infrastrukturelemente für den Radverkehr. Online unter: <a class="text-blue-800 hover:underline" href="https://repository.difu.de/jspui/bitstream/difu/256760/1/DS1918.pdf">https://repository.difu.de/jspui/bitstream/difu/256760/1/DS1918.pdf</a> (abgerufen 09.09.21)<br><br>
+                UMWELTBUNDESAMT (2021): Vorteile des Fahrradfahrens. Online unter: <a class="text-blue-800 hover:underline" href="https://www.umweltbundesamt.de/themen/verkehr-laerm/nachhaltige-mobilitaet/radverkehr#gtgt-schnell">https://www.umweltbundesamt.de/themen/verkehr-laerm/nachhaltige-mobilitaet/radverkehr#gtgt-schnell</a> (abgerufen 09.09.21)<br><br>
+                VELO REGION (o. S.): Fahrradstraßen 2.0 in Münster. Online unter: <a class="text-blue-800 hover:underline" href="https://www.veloregion.de/hintergrund/verkehrsplanung/fahrradstrassen-2.0-in-muenster/">https://www.veloregion.de/hintergrund/verkehrsplanung/fahrradstrassen-2.0-in-muenster/</a> (abgerufen 09.09.21)<br><br>
+                VELTOP (o. S.): Fahrrad vs Auto. Online unter: <a class="text-blue-800 hover:underline" href="https://www.veltop.eu/de/blog/post/fahrrad-vs-auto.html">https://www.veltop.eu/de/blog/post/fahrrad-vs-auto.html</a> (abgerufen 09.09.21)
+            </div>
+        </div>
+    </section>
+    <a class="text-blue-800 hover:underline" href=""></a>
 </x-guest-layout>
