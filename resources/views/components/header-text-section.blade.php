35f31129475a490f6a55f8f936d7b8b97dfc6eb7
Benni Dietz <b_diet05@uni-muenster.de>
2021-09-14 10:47:10 +0200

    Include mobility sites

--- resources/views/components/header-text-section.blade.php
+++ resources/views/components/header-text-section.blade.php
@@ -2,7 +2,7 @@
     <section class="pt-24 max-w-7xl mx-auto">
         <h2 class="mb-4 text-3xl font-bold uppercase tracking-wider text-green-600">{{ $header }}</h2>
         <div class="text-xl w-2/3 leading-relaxed tracking-wide">
-            {{ $text }}
+            {!! $text !!}
         </div>
     </section>
 </div>
