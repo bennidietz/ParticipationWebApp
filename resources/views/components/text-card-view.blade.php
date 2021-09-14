d329faac5c2920f7951e9d816c64635563be7724
Benni Dietz <b_diet05@uni-muenster.de>
2021-09-14 12:15:54 +0200

    Image fixes

--- resources/views/components/text-card-view.blade.php
+++ resources/views/components/text-card-view.blade.php
@@ -1,13 +1,13 @@
 <div>
-    <div>
+    <section class="m-6">
         <div class="mt-16 py-4 px-4 bg-whit w-72 bg-white rounded-xl shadow-lg hover:shadow-xl transform hover:scale-110 transition duration-500 mx-auto md:mx-0">
             <div class="w-sm">
-                <img class="w-64" src="{{ $imageUrl }}" alt="" />
+                <img class="w-64" src="{{ $imageUrl }}" height="500" alt="" />
                 <div class="mt-4 text-green-600 text-center">
                     <h1 class="text-xl font-bold">{{ $text }}</h1>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 
 </div>
